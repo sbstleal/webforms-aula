9017b5253bbf2c9832053293758c309c0688a447
sbstleal <sebastiaodeoleal@gmail.com>
2021-08-07 12:46:09 -0300

    webforms e asp.net

---
+++ WebForms/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForms
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            lblTeste.Text = "Olá pessoal";
+            dvTeste.InnerHtml = "olá pessoal do html";
+        }
+    }
+}
